a6058cbe3eca2c9bea9072c8d37282c1024de51c
EVE <andreve40@gmail.com>
2019-10-25 23:38:03 +0200

    ok

--- mini_projet/prog_calcul_routier/algo.py
+++ mini_projet/prog_calcul_routier/algo.py
@@ -1,34 +1,35 @@
+from dic import villes
+import math
+
 # Vitesse maximale en km/h.
 vit_max = 90
 # Temps d'une pause
 pause = 15
-# Vitesse accelération en km/h par minutes.
-accelerer = 10
-# Vitesse descelération en km/h par minutes.
-ralentir = 10
-# Temps du parcours en minutes.
-temps = 0
 # Temps pour arriver à la vitesse maximale (90 km/h) en minutes.
 temps_vit_max = 9
-# Tableau d'affichage.
-tab = []
+temps = 0
 
 # On demande de saisir une ville de départ et une ville d'arrivée.
-vil_dep = input("Choisir une ville de départ : ")
-vil_arr = input("Choisir une ville d'arrivée : ")
+vil_dep = input("Choisir une ville de départ : \n (Bordeaux-Bayonne-Lille-Mulhouse-Orthez- \n Pau-Paris-Poitiers-Royan-Toulouse) \n ")
+vil_arr = input("Choisir une ville d'arrivée : \n (Bordeaux-Bayonne-Lille-Mulhouse-Orthez- \n Pau-Paris-Poitiers-Royan-Toulouse) \n ")
 # Algo pour la saisir 2 villes différentes.
 while vil_dep == vil_arr :
+    print('Erreur ! Veuillez réessayer.')
     vil_dep = input("Choisir une ville de départ : ")
     vil_arr = input("Choisir une ville d'arrivée : ")
 
-# Déterminer le temps de toutes les pauses.
-if (temps/2)%2 == 0 :
-    temps_pause = (temps/2) * pause + temps_vit_max * 2
-# Déterminer le temps du trajet sans pauses.
-temps_sans_pause = distance/vitesse_max
-# Déterminer le temps total.
-temps_total = temps_pause + temps_sans_pause + temps
+# Tableau d'affichage.
+tab = []
+distance = villes[vil_dep][vil_arr]
 
-# Ligne pour remplir le tableau.
-tab.extend((vil_dep, vil_arr, distance, temps))
+if vil_dep != vil_arr :
+    # Déterminer le temps de toutes les pauses.
+    if (temps/2)%2 == 0 :
+        temps_pause = (temps/2) * pause + temps_vit_max * 2
+    # Déterminer le temps du trajet sans pauses.
+    temps_sans_pause = distance/vit_max
+    # Déterminer le temps total.
+    temps_total = temps_pause + temps_sans_pause + temps_vit_max
+    # Ligne pour remplir le tableau.
+    tab.extend((vil_dep, vil_arr, distance, temps_total))
 print(tab)
---
+++ mini_projet/prog_calcul_routier/dic.py
@@ -0,0 +1,77 @@
+villes = {
+    "Bordeaux": {
+        "Bayonne": 184,        
+        "Lille": 801,
+        "Mulhouse": 832,
+        "Orthez": 191,
+        "Paris": 588,
+        "Pau": 217,
+        "Poitiers": 250,
+        "Royan": 131,
+        "Toulouse": 245
+    },
+
+    "Bayonne": {
+        "Lille": 986,
+        "Mulhouse": 1013,
+        "Orthez": 74,
+        "Pau": 112,
+        "Paris": 771,
+        "Poitiers": 435,
+        "Royan": 319,
+        "Toulouse": 300
+    },
+
+    "Lille": {
+        "Pau": 1010,
+        "Mulhouse": 595,
+        "Orthez": 1028,
+        "Paris": 225,
+        "Poitiers": 561,
+        "Royan": 727,
+        "Toulouse": 894
+    },
+
+    "Mulhouse": {
+        "Pau": 1039,
+        "Orthez": 1057,
+        "Paris": 538,
+        "Poitiers": 748,
+        "Toulouse": 836,
+        "Royan": 915
+    },
+
+    "Orthez": {
+        "Pau": 47,
+        "Paris": 770,
+        "Poitiers": 479,
+        "Royan": 363,
+        "Toulouse": 234
+    },
+
+    "Pau": {
+        "Paris": 796,
+        "Poitiers": 461,
+        "Royan": 344,
+        "Toulouse": 199
+    },
+
+    "Paris": {
+        "Poitiers": 345,
+        "Royan": 178,
+        "Toulouse": 489
+    },
+
+    # Poitiers est deja fait
+    "Poitiers": {
+        "Royan": 178,
+        "Toulouse": 489,
+    },
+
+    "Royan": {
+        "Toulouse": 371,
+    },
+    "Toulouse":{
+        
+    }
+}
